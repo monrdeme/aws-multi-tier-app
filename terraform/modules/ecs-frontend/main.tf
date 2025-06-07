5406fcde9729892a50ec2e0f67a3144d4f516761
monrdeme <dmonroe1026@gmail.com>
2025-06-07 06:40:12 -0700

    Update ecs frontend task definition and increase timeout and interval for health checks

--- terraform/modules/ecs-frontend/main.tf
+++ terraform/modules/ecs-frontend/main.tf
@@ -302,8 +302,8 @@ resource "aws_lb_target_group" "frontend_app" {
     path                = "/health" # Health check endpoint in Flask app
     protocol            = "HTTP"
     matcher             = "200"
-    interval            = 30
-    timeout             = 5
+    interval            = 60
+    timeout             = 15
     healthy_threshold   = 2
     unhealthy_threshold = 2
   }
